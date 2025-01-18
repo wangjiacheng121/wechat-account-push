7ed9ca9a59fcba6abf2cc6fe8e1146d663dd819a
wangjiacheng121 <132195885+wangjiacheng121@users.noreply.github.com>
2025-01-18 15:53:14 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,1 +1,51 @@
-{"APP_ID":"wxcefc46b918e71299","APP_SECRET":"0a1a5fc69ddf493876d1ceb88f8a7dd7","IS_SHOW_COLOR":false,"CALLBACK_TEMPLATE_ID":"jyNYiufIBIreItAJrVweyfH8CEGgX7BG_35w_ygTw9g","CALLBACK_USERS":[{"name":"自己","id":"oDAVL7Ij93TuXRhjHtoGf18DWbkA"}],"USERS":[{"name":"卓宝","id":"oDAVL7Ij93TuXRhjHtoGf18DWbkA","useTemplateId":"McXwP3M58bzoZ4Lvxfux_CEvrU9cPcDN7kep6jlIp2k","province":"河北","city":"张家口","horoscopeDate":"02-20","horoscopeDateType":"今日","openUrl":"https://shuangxunian.github.io/","festivals":[{"type":"*生日","name":"卓卓生日","date":"02-23","year":"2025"},{"type":"*生日","name":"笑笑生日","date":"01-25","year":"2025"}],"customizedDateList":[]}],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":true,"oneTalk":true,"earthyLoveWords":false,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":true,"horoscope":true,"birthdayMessage":true}}
+module.exports = {
+  "APP_ID": "wxcefc46b918e71299",
+  "APP_SECRET": "0a1a5fc69ddf493876d1ceb88f8a7dd7",
+  "IS_SHOW_COLOR": false,
+  "CALLBACK_TEMPLATE_ID": "jyNYiufIBIreItAJrVweyfH8CEGgX7BG_35w_ygTw9g",
+  "CALLBACK_USERS": [
+    {
+      "name": "自己",
+      "id": "oDAVL7Ij93TuXRhjHtoGf18DWbkA"
+    }
+  ],
+  "USERS": [
+    {
+      "name": "卓宝",
+      "id": "oDAVL7Ij93TuXRhjHtoGf18DWbkA",
+      "useTemplateId": "McXwP3M58bzoZ4Lvxfux_CEvrU9cPcDN7kep6jlIp2k",
+      "province": "河北",
+      "city": "张家口",
+      "horoscopeDate": "02-20",
+      "horoscopeDateType": "今日",
+      "openUrl": "https://shuangxunian.github.io/",
+      "festivals": [
+        {
+          "type": "*生日",
+          "name": "卓卓生日",
+          "date": "02-23",
+          "year": "2025"
+        },
+        {
+          "type": "*生日",
+          "name": "笑笑生日",
+          "date": "01-25",
+          "year": "2025"
+        }
+      ],
+      "customizedDateList": []
+    }
+  ],
+  "SWITCH": {
+    "weather": true,
+    "holidaytts": true,
+    "CIBA": true,
+    "oneTalk": true,
+    "earthyLoveWords": false,
+    "momentCopyrighting": false,
+    "poisonChickenSoup": false,
+    "poetry": true,
+    "horoscope": true,
+    "birthdayMessage": true
+  }
+};
