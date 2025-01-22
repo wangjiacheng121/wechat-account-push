6c87a98ea122c331796f3ffbbca33982f6b3f404
wangjiacheng121 <132195885+wangjiacheng121@users.noreply.github.com>
2025-01-22 15:53:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,20 +1,21 @@
 // config.js
 module.exports = {
+  USE_PASSAGE: 'server-chan',
   "APP_ID": "wxcefc46b918e71299",
   "APP_SECRET": "cf8ef6c48e6f469fc965ec17336b3a11",
   "IS_SHOW_COLOR": false,
-  "CALLBACK_TEMPLATE_ID": "jyNYiufIBIreItAJrVweyfH8CEGgX7BG_35w_ygTw9g",
+  "CALLBACK_TEMPLATE_ID": "0002",
   "CALLBACK_USERS": [
     {
       "name": "自己",
-      "id": "oDAVL7Ij93TuXRhjHtoGf18DWbkA"
+      "id": "SCT268131THz7Yj7Mi2ykML0doe3TfQDxu"
     }
   ],
   "USERS": [
     {
       "name": "卓宝",
-      "id": "oDAVL7E_4jCxxirmcHQSyYWBpuyU",
-      "useTemplateId": "_G7oR9K2obN6hNO2ljqFiDJek6T0Ub2uOp1K1smzL14",
+      "id": "SCT268131THz7Yj7Mi2ykML0doe3TfQDxu",
+      "useTemplateId": "0001",
       "province": "河北省",
       "city": "张家口",
       "horoscopeDate": "02-20",
@@ -43,8 +44,8 @@ module.exports = {
     },
     {
       "name": "笑猫",
-      "id": "oDAVL7Ij93TuXRhjHtoGf18DWbkA",
-      "useTemplateId": "_G7oR9K2obN6hNO2ljqFiDJek6T0Ub2uOp1K1smzL14",
+      "id": "SCT268131THz7Yj7Mi2ykML0doe3TfQDxu",
+      "useTemplateId": "0001",
       "province": "河北省",
       "city": "张家口",
       "horoscopeDate": "02-20",
